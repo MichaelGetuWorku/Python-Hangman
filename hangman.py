ce402d42db42eb99264e931c7148901b2006cf32
MichaelGetuWorku <michael.getu.hh3221@gmail.com>
2021-07-22 18:16:17 +0300

    Modified Step1

--- hangman.py
+++ hangman.py
@@ -4,16 +4,18 @@ import random
 word_list = ['ardvark', 'baboon', 'camel']
 
 # TODO:1 Randomly choose a word from the wordList and assign it to a variable calles chosen_word
-chosen_word = random.randint(0, len(word_list) - 1)
-loopWord = word_list[chosen_word]
+chosen_word = random.choice(word_list)
+# or
+# chosen_word = random.randint(0, len(word_list) - 1)
+# loopWord = word_list[chosen_word]
 
 # TODO:2 Ask the user to guess letter and assign thire answer to a variable called gusse. make guess lowercase.
 guess = input('Guess the mistry letter: ').lower()
 
 # TODO:3 check if the letter the user gussed is one of the letters in the chosen_word
 
-for look_letter in loopWord:
-    if guess == look_letter:
+for letter in chosen_word:
+    if guess == letter:
         print('Right')
     else:
       print('Wrong')
